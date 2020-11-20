31aef32a1826ede8281009f21a02276e829d4813
ardel <71622783+ardeliaraminta@users.noreply.github.com>
2020-11-20 22:57:35 +0700

    Add files via upload
    my first add

---
+++ #Exercise 2.py
@@ -0,0 +1,24 @@
+#Exercise 2 
+import math
+
+class Circle:
+
+    def __init__(self, radius = 1.0, colour = "red")
+    self.radius = radius
+    self.colour = colour 
+
+    def getRadius(self):
+        return self.radius = radius 
+    
+    def setRadius(self,radius):
+        self.radius = radius  
+
+    def getColour(self):
+        return self.colour
+
+    def setColour(self, colour):
+        self.colour = colour 
+
+    def __str__(self):
+        
+
